a1b37e5e1d8203c0e3744b21e9a4ac93f0e73109
Zac H <zac.haluza@gmail.com>
2020-05-16 23:21:39 -0400

    Basic product item template

---
+++ components/shop/ProductItem.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import { View, Text, Image, Button, StyleSheet } from 'react-native';
+import Colors from '../../constants/Colors';
+
+const ProductItem = ({ image, title, price, onViewDetail, onAddToCart }) => {
+  return (
+    <View style={styles.product}>
+      <View style={styles.imageContainer}>
+        <Image style={styles.image} source={{ uri: image }} />
+      </View>
+
+      <View style={styles.details}>
+        <Text style={styles.title}>{title}</Text>
+        <Text style={styles.price}>${price.toFixed(2)}</Text>
+      </View>
+
+      <View style={styles.buttons}>
+        <Button
+          color={Colors.primary}
+          title="View Details"
+          onPress={onViewDetail}
+        />
+        <Button
+          color={Colors.primary}
+          title="Add to Cart"
+          onPress={onAddToCart}
+        />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  product: {
+    shadowColor: 'black',
+    shadowOpacity: 0.26,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
+    elevation: 5,
+    borderRadius: 10,
+    backgroundColor: 'white',
+    height: 300,
+    margin: 20,
+  },
+  imageContainer: {
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    overflow: 'hidden',
+    height: '60%',
+    width: '100%',
+  },
+  image: {
+    height: '100%',
+    width: '100%',
+  },
+  details: {
+    alignItems: 'center',
+    height: '15%',
+    padding: 5,
+  },
+  title: {
+    fontSize: 18,
+    marginVertical: 4,
+  },
+  price: {
+    fontSize: 14,
+    color: Colors.grayText,
+  },
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: '25%',
+    paddingHorizontal: 20,
+  },
+});
+
+export default ProductItem;
