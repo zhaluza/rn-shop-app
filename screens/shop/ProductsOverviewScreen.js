19730f9f34c920584de7365c2e2ac6f0bc26ae8c
Zac H <zac.haluza@gmail.com>
2020-05-16 23:22:01 -0400

    include and display ProductItem component

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -2,12 +2,22 @@ import React from 'react';
 import { FlatList, Text } from 'react-native';
 import { useSelector } from 'react-redux';
 
+import ProductItem from '../../components/shop/ProductItem';
+
 const ProductOverviewScreen = (props) => {
   const products = useSelector((state) => state.products.availableProducts);
   return (
     <FlatList
       data={products}
-      renderItem={(itemData) => <Text>{itemData.item.title}</Text>}
+      renderItem={(itemData) => (
+        <ProductItem
+          image={itemData.item.imageUrl}
+          title={itemData.item.title}
+          price={itemData.item.price}
+          onViewDetail={() => {}}
+          onAddToCart={() => {}}
+        />
+      )}
     />
   );
 };
