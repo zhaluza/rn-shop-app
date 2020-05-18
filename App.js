a3b7bf7785e25a9af167ae6c122c9b42185a55e5
Zac H <zac.haluza@gmail.com>
2020-05-18 09:00:38 -0400

    added custom fonts

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
+import { useFonts } from '@use-expo/font';
+import { AppLoading } from 'expo';
 
 import productsReducer from './store/reducers/products';
 import ShopNavigator from './navigation/ShopNavigator';
@@ -12,9 +14,18 @@ const rootReducer = combineReducers({
 const store = createStore(rootReducer);
 
 export default function App() {
-  return (
-    <Provider store={store}>
-      <ShopNavigator />
-    </Provider>
-  );
+  let [fontsLoaded] = useFonts({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <Provider store={store}>
+        <ShopNavigator />
+      </Provider>
+    );
+  }
 }
