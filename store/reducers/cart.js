5494b4a01eb2f1cc565ed1b254f317470d55c301
Zac H <zac.haluza@gmail.com>
2020-05-20 08:42:33 -0400

    renamed "itemsInCart" and "productTitle"

--- store/reducers/cart.js
+++ store/reducers/cart.js
@@ -2,39 +2,37 @@ import { ADD_TO_CART } from '../actions/cart';
 import CartItem from '../../models/cart-item';
 
 const initialState = {
-  itemsInCart: {},
+  items: {},
   total: 0,
 };
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case ADD_TO_CART:
-      const addedProduct = action.addedProduct;
+      const addedProduct = action.product;
       const productId = addedProduct.id;
       const productPrice = addedProduct.price;
-      const productTitle = addedProduct.title;
+      const productName = addedProduct.title;
 
       let cartItem;
 
-      if (state.itemsInCart[productId]) {
+      if (state.items[productId]) {
         // item is already in cart
-        cartItem = newCartItem(
-          state.itemsInCart[productId].quantity + 1,
+        cartItem = new CartItem(
+          state.items[productId].quantity + 1,
           productPrice,
-          productTitle,
-          state.itemsInCart[productId].sum + productPrice
+          productName,
+          state.items[productId].sum + productPrice
         );
       } else {
         // add item to cart
-        cartItem = new CartItem(1, productPrice, productTitle, productPrice);
+        cartItem = new CartItem(1, productPrice, productName, productPrice);
       }
       return {
         ...state,
-        itemsInCart: { ...state.itemsInCart, [productId]: cartItem },
+        items: { ...state.items, [productId]: cartItem },
         total: state.total + productPrice,
       };
-
-    default:
-      return state;
   }
+  return state;
 };
