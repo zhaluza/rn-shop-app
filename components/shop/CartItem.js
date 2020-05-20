d413690c2f9d665faec4ee47c5a3d86ffbc7512a
Zac H <zac.haluza@gmail.com>
2020-05-20 08:39:46 -0400

    Created CartItem component

---
+++ components/shop/CartItem.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Platform,
+} from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+
+import Colors from '../../constants/Colors';
+
+const CartItem = ({ quantity, name, amount, onRemove }) => {
+  return (
+    <View style={styles.cartItem}>
+      <View style={styles.itemData}>
+        <Text style={styles.quantity}>{quantity}</Text>
+        <Text style={styles.mainText}>{name}</Text>
+      </View>
+      <View style={styles.itemData}>
+        <Text style={styles.mainText}>${amount.toFixed(2)}</Text>
+        <TouchableOpacity onPress={onRemove} style={styles.deleteButton}>
+          <Ionicons
+            name={Platform.OS === 'android' ? 'md-trash' : 'ios-trash'}
+            size={23}
+            color="red"
+          />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+const fontSize = 16;
+const styles = StyleSheet.create({
+  cartItem: {
+    padding: 10,
+    backgroundColor: 'white',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginHorizontal: 20,
+  },
+  itemData: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  quantity: {
+    fontFamily: 'open-sans',
+    color: Colors.grayText,
+    marginRight: 7,
+    fontSize,
+  },
+  mainText: {
+    fontFamily: 'open-sans-bold',
+    fontSize,
+  },
+  deleteButton: {
+    marginLeft: 20,
+  },
+});
+
+export default CartItem;
