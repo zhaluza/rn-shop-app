1cc26d6ce7ead5d4e2f3ffd9aa7993aafea8217c
Zac H <zac.haluza@gmail.com>
2020-05-21 09:35:17 -0400

    basic orders screen

---
+++ screens/shop/OrdersScreen.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { FlatList, Text, Platform } from 'react-native';
+import { useSelector } from 'react-redux';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+
+import HeaderButton from '../../components/UI/HeaderButton';
+
+const OrdersScreen = (props) => {
+  const orders = useSelector((state) => state.orders.orders);
+  return (
+    <FlatList
+      data={orders}
+      renderItem={(itemData) => <Text>{itemData.item.total}</Text>}
+    />
+  );
+};
+
+OrdersScreen.navigationOptions = (navData) => {
+  return {
+    headerLeft: () => (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Cart"
+          iconName={Platform.OS === 'android' ? 'md-menu' : 'ios-menu'}
+          onPress={() => {
+            navData.navigation.toggleDrawer();
+          }}
+        />
+      </HeaderButtons>
+    ),
+    headerTitle: 'Your Orders',
+  };
+};
+
+export default OrdersScreen;
