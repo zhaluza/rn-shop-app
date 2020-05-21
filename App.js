b4db73d39cdbd373073793ed86a0d174b3e22dd3
Zac H <zac.haluza@gmail.com>
2020-05-21 09:18:30 -0400

    added ordersReducer

--- App.js
+++ App.js
@@ -6,11 +6,13 @@ import { AppLoading } from 'expo';
 
 import productsReducer from './store/reducers/products';
 import cartReducer from './store/reducers/cart';
+import ordersReducer from './store/reducers/orders';
 import ShopNavigator from './navigation/ShopNavigator';
 
 const rootReducer = combineReducers({
   products: productsReducer,
   cart: cartReducer,
+  orders: ordersReducer,
 });
 
 const store = createStore(rootReducer);
