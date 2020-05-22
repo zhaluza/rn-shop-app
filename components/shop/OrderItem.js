4b64c5c568a15740364b9a87716f9c79e0d911ee
Zac H <zac.haluza@gmail.com>
2020-05-21 23:43:16 -0400

    created OrderItem component

---
+++ components/shop/OrderItem.js
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+import CartItem from './CartItem';
+import Colors from '../../constants/Colors';
+
+const OrderItem = ({ total, date, items }) => {
+  console.log(items);
+  const [showDetails, setShowDetails] = useState(false);
+  return (
+    <View style={styles.orderItem}>
+      <View style={styles.summary}>
+        <Text style={styles.total}>${total.toFixed(2)}</Text>
+        <Text style={styles.date}>{date}</Text>
+      </View>
+      <Button
+        color={Colors.primary}
+        title="Show Details"
+        onPress={() => setShowDetails((prevState) => !prevState)}
+      />
+      {showDetails && (
+        <View>
+          {items.map((cartItem) => (
+            <CartItem
+              quantity={cartItem.quantity}
+              amount={cartItem.sum}
+              name={cartItem.productName}
+              deletable={false}
+            />
+          ))}
+        </View>
+      )}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  orderItem: {
+    shadowColor: 'black',
+    shadowOpacity: 0.26,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
+    elevation: 5,
+    borderRadius: 10,
+    backgroundColor: 'white',
+    margin: 20,
+    padding: 10,
+    alignItems: 'center',
+  },
+  summary: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '100%',
+    marginBottom: 15,
+  },
+  total: {
+    fontFamily: 'open-sans-bold',
+    fontSize: 16,
+  },
+  date: {
+    fontSize: 16,
+    fontFamily: 'open-sans',
+    color: Colors.grayText,
+  },
+});
+
+export default OrderItem;
