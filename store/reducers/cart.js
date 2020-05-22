8986dab6760973415a2ec28330ed7006a195920e
Zac H <zac.haluza@gmail.com>
2020-05-22 11:29:32 -0400

    incorporated delete product logic

--- store/reducers/cart.js
+++ store/reducers/cart.js
@@ -1,5 +1,6 @@
 import { ADD_TO_CART, REMOVE_FROM_CART } from '../actions/cart';
 import { ADD_ORDER } from '../actions/orders';
+import { DELETE_PRODUCT } from '../actions/products';
 import CartItem from '../../models/cart-item';
 
 const initialState = {
@@ -59,10 +60,22 @@ export default (state = initialState, action) => {
       return {
         ...state,
         items: updatedCartItems,
-        total: Math.abs(state.total - selectedCartItem.productPrice),
+        total: state.total - selectedCartItem.productPrice,
       };
     case ADD_ORDER:
       return initialState;
+    case DELETE_PRODUCT:
+      if (!state.items[action.pid]) {
+        return state;
+      }
+      const updatedItems = { ...state.items };
+      const itemTotal = state.items[action.pid].sum;
+      delete updatedItems[action.pid];
+      return {
+        ...state,
+        items: items.filter((product) => {}),
+        total: state.total - itemTotal,
+      };
   }
   return state;
 };
