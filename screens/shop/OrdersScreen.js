4198df529b450486da7bcc4cd757af3217a1371e
Zac H <zac.haluza@gmail.com>
2020-05-21 23:44:34 -0400

    fleshed out rendered data

--- screens/shop/OrdersScreen.js
+++ screens/shop/OrdersScreen.js
@@ -4,13 +4,20 @@ import { useSelector } from 'react-redux';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
 import HeaderButton from '../../components/UI/HeaderButton';
+import OrderItem from '../../components/shop/OrderItem';
 
 const OrdersScreen = (props) => {
   const orders = useSelector((state) => state.orders.orders);
   return (
     <FlatList
       data={orders}
-      renderItem={(itemData) => <Text>{itemData.item.total}</Text>}
+      renderItem={(itemData) => (
+        <OrderItem
+          items={itemData.item.items}
+          total={itemData.item.total}
+          date={itemData.item.readableDate}
+        />
+      )}
     />
   );
 };
