688cddb2432c20473c9c6055f6e9d250586a9755
Zac H <zac.haluza@gmail.com>
2020-05-21 23:43:30 -0400

    Use Moment to format date

--- models/order.js
+++ models/order.js
@@ -1,9 +1,14 @@
+import moment from 'moment';
+
 class Order {
   constructor(id, items, total, date) {
     this.id = id;
     this.items = items;
     this.total = total;
     this.date = date;
+  }
+  get readableDate() {
+    return moment(this.date).format('MMMM Do YYYY, hh:mm');
   }
 }
 export default Order;
