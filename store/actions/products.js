ca5d8fa719b4c1b02eaccb61ce88f274fa1a72b2
Zac H <zac.haluza@gmail.com>
2020-05-23 11:04:38 -0400

    added create and update product actions

--- store/actions/products.js
+++ store/actions/products.js
@@ -1,5 +1,31 @@
 export const DELETE_PRODUCT = 'DELETE_PRODUCT';
+export const CREATE_PRODUCT = 'CREATE_PRODUCT';
+export const UPDATE_PRODUCT = 'UPDATE_PRODUCT';
 
 export const deleteProduct = (productId) => {
   return { type: DELETE_PRODUCT, pid: productId };
+};
+
+export const createProduct = (name, description, imageUrl, price) => {
+  return {
+    type: CREATE_PRODUCT,
+    productData: {
+      name,
+      description,
+      imageUrl,
+      price,
+    },
+  };
+};
+
+export const updateProduct = (id, name, description, imageUrl) => {
+  return {
+    type: UPDATE_PRODUCT,
+    pid: id,
+    productData: {
+      name,
+      description,
+      imageUrl,
+    },
+  };
 };
