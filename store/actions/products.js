c53a5c5d81179b6a90dbdd34a8abe44c9776f4ab
Zac H <zac.haluza@gmail.com>
2020-06-06 10:45:46 -0400

    Interacts with firebase db to add/fetch products

--- store/actions/products.js
+++ store/actions/products.js
@@ -1,20 +1,70 @@
+import Product from '../../models/product';
+
 export const DELETE_PRODUCT = 'DELETE_PRODUCT';
 export const CREATE_PRODUCT = 'CREATE_PRODUCT';
 export const UPDATE_PRODUCT = 'UPDATE_PRODUCT';
+export const SET_PRODUCTS = 'SET_PRODUCTS';
+
+export const fetchProducts = () => {
+  return async (dispatch) => {
+    // execute asynchronous code here
+    const response = await fetch(
+      'https://rn-shopping-app-af32b.firebaseio.com/products.json'
+    );
+
+    const resData = await response.json();
+    const loadedProducts = [];
+    for (const key in resData) {
+      loadedProducts.push(
+        new Product(
+          key,
+          'u1',
+          resData[key].name,
+          resData[key].imageUrl,
+          resData[key].description,
+          resData[key].price
+        )
+      );
+    }
+    dispatch({ type: SET_PRODUCTS, products: loadedProducts });
+  };
+};
 
 export const deleteProduct = (productId) => {
   return { type: DELETE_PRODUCT, pid: productId };
 };
 
 export const createProduct = (name, description, imageUrl, price) => {
-  return {
-    type: CREATE_PRODUCT,
-    productData: {
-      name,
-      description,
-      imageUrl,
-      price,
-    },
+  return async (dispatch) => {
+    // execute asynchronous code here
+    const response = await fetch(
+      'https://rn-shopping-app-af32b.firebaseio.com/products.json',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          name,
+          description,
+          imageUrl,
+          price,
+        }),
+      }
+    );
+
+    const resData = await response.json();
+
+    dispatch({
+      type: CREATE_PRODUCT,
+      productData: {
+        id: resData.name,
+        name,
+        description,
+        imageUrl,
+        price,
+      },
+    });
   };
 };
 
