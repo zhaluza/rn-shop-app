cf11259feaf323720c04a0f96d33660f97faafbf
Zac H <zac.haluza@gmail.com>
2020-06-06 11:30:20 -0400

    added error handling

--- store/actions/products.js
+++ store/actions/products.js
@@ -7,26 +7,35 @@ export const SET_PRODUCTS = 'SET_PRODUCTS';
 
 export const fetchProducts = () => {
   return async (dispatch) => {
-    // execute asynchronous code here
-    const response = await fetch(
-      'https://rn-shopping-app-af32b.firebaseio.com/products.json'
-    );
-
-    const resData = await response.json();
-    const loadedProducts = [];
-    for (const key in resData) {
-      loadedProducts.push(
-        new Product(
-          key,
-          'u1',
-          resData[key].name,
-          resData[key].imageUrl,
-          resData[key].description,
-          resData[key].price
-        )
+    try {
+      const response = await fetch(
+        'https://rn-shopping-app-af32b.firebaseio.com/products.json'
       );
+
+      if (!response.ok) {
+        throw new Error(
+          'Something went wrong with the response from the server!'
+        );
+      }
+
+      const resData = await response.json();
+      const loadedProducts = [];
+      for (const key in resData) {
+        loadedProducts.push(
+          new Product(
+            key,
+            'u1',
+            resData[key].name,
+            resData[key].imageUrl,
+            resData[key].description,
+            resData[key].price
+          )
+        );
+      }
+      dispatch({ type: SET_PRODUCTS, products: loadedProducts });
+    } catch (err) {
+      throw err;
     }
-    dispatch({ type: SET_PRODUCTS, products: loadedProducts });
   };
 };
 
