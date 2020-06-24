392ed26e00ad5ba021059c84aa636c3c5895c6bf
Zac H <zac.haluza@gmail.com>
2020-06-23 23:20:29 -0400

    can now add orders to db

--- store/actions/orders.js
+++ store/actions/orders.js
@@ -1,8 +1,36 @@
 export const ADD_ORDER = 'ADD_ORDER';
 
 export const addOrder = (cartItems, total) => {
-  return {
-    type: ADD_ORDER,
-    orderData: { items: cartItems, total: total },
+  return async (dispatch) => {
+    const date = new Date();
+    const response = await fetch(
+      'https://rn-shopping-app-af32b.firebaseio.com/orders/u1.json',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          cartItems,
+          total,
+          date: date.toISOString(),
+        }),
+      }
+    );
+    if (!response.ok) {
+      throw new Error('Something went wrong');
+    }
+
+    const resData = await response.json();
+
+    dispatch({
+      type: ADD_ORDER,
+      orderData: {
+        id: resData.name,
+        items: cartItems,
+        amount: total,
+        date: date,
+      },
+    });
   };
 };
