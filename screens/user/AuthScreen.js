4a43463de609cdc6f0ab01232cc110e88c069de0
Zac H <zac.haluza@gmail.com>
2020-06-28 22:43:27 -0400

    Basic auth screen

---
+++ screens/user/AuthScreen.js
@@ -0,0 +1,95 @@
+import React from 'react';
+import {
+  ScrollView,
+  StyleSheet,
+  View,
+  KeyboardAvoidingView,
+  Button,
+} from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+
+import Input from '../../components/UI/Input';
+import Card from '../../components/UI/Card';
+import Colors from '../../constants/Colors';
+
+const AuthScreen = (props) => {
+  return (
+    <KeyboardAvoidingView
+      behavior="padding"
+      keyboardVerticalOffset={50}
+      style={styles.screen}
+    >
+      <LinearGradient colors={['#ffedff', '#ffe3ff']} style={styles.gradient}>
+        <Card style={styles.authContainer}>
+          <ScrollView>
+            <Input
+              id="email"
+              label="Email"
+              keyboardType="email-address"
+              required
+              email
+              autoCapitalize="none"
+              errorMessage="Please enter a valid email address"
+              onInputChange={() => {}}
+              initialValue=""
+            />
+            <Input
+              id="password"
+              label="password"
+              keyboardType="default"
+              secureTextEntry
+              required
+              minLength={5}
+              autoCapitalize="none"
+              errorMessage="Please enter a valid password"
+              onInputChange={() => {}}
+              initialValue=""
+            />
+            <View style={styles.buttonContainer}>
+              <Button
+                title="Log in"
+                color={Colors.primary}
+                onPress={() => {}}
+              />
+            </View>
+
+            <View style={styles.buttonContainer}>
+              <Button
+                title="Switch to Signup"
+                color={Colors.accent}
+                onPress={() => {}}
+              />
+            </View>
+          </ScrollView>
+        </Card>
+      </LinearGradient>
+    </KeyboardAvoidingView>
+  );
+};
+
+AuthScreen.navigationOptions = {
+  headerTitle: 'Log In',
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+  },
+  authContainer: {
+    flex: 1,
+    width: '80%',
+    maxWidth: 400,
+    maxHeight: 400,
+    padding: 20,
+  },
+  gradient: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonContainer: {
+    marginTop: 10,
+  },
+});
+
+export default AuthScreen;
