6eab2119e6d51283a6a40b8022ed6a126315fdfa
Zac H <zac.haluza@gmail.com>
2020-10-04 21:39:07 -0400

    include user token with requests

--- store/actions/products.js
+++ store/actions/products.js
@@ -8,14 +8,10 @@ export const SET_PRODUCTS = 'SET_PRODUCTS';
 export const fetchProducts = () => {
   return async (dispatch) => {
     try {
-      const response = await fetch(
-        'https://rn-shopping-app-af32b.firebaseio.com/products.json'
-      );
+      const response = await fetch('https://rn-shopping-app-af32b.firebaseio.com/products.json');
 
       if (!response.ok) {
-        throw new Error(
-          'Something went wrong with the response from the server!'
-        );
+        throw new Error('Something went wrong with the response from the server!');
       }
 
       const resData = await response.json();
@@ -40,9 +36,10 @@ export const fetchProducts = () => {
 };
 
 export const deleteProduct = (productId) => {
-  return async (dispatch) => {
+  return async (dispatch, getState) => {
+    const token = getState().auth.token;
     const response = await fetch(
-      `https://rn-shopping-app-af32b.firebaseio.com/products/${productId}.json`,
+      `https://rn-shopping-app-af32b.firebaseio.com/products/${productId}.json?auth=${token}`,
       {
         method: 'DELETE',
       }
@@ -55,10 +52,10 @@ export const deleteProduct = (productId) => {
 };
 
 export const createProduct = (name, description, imageUrl, price) => {
-  return async (dispatch) => {
-    // execute asynchronous code here
+  return async (dispatch, getState) => {
+    const token = getState().auth.token;
     const response = await fetch(
-      'https://rn-shopping-app-af32b.firebaseio.com/products.json',
+      `https://rn-shopping-app-af32b.firebaseio.com/products.json?auth=${token}`,
       {
         method: 'POST',
         headers: {
@@ -89,9 +86,10 @@ export const createProduct = (name, description, imageUrl, price) => {
 };
 
 export const updateProduct = (id, name, description, imageUrl) => {
-  return async (dispatch) => {
+  return async (dispatch, getState) => {
+    const token = getState().auth.token;
     const response = await fetch(
-      `https://rn-shopping-app-af32b.firebaseio.com/products/${id}.json`,
+      `https://rn-shopping-app-af32b.firebaseio.com/products/${id}.json?auth=${token}`,
       {
         method: 'PATCH',
         headers: {
