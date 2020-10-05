f0e0216e0e8b229cab5238393ad76639f7dd01b8
Zac H <zac.haluza@gmail.com>
2020-10-05 00:38:48 -0400

    create auto-login component

---
+++ screens/StartupScreen.js
@@ -0,0 +1,46 @@
+import React, { useEffect } from 'react';
+import { View, ActivityIndicator, StyleSheet } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
+import { useDispatch } from 'react-redux';
+
+import Colors from '../constants/Colors';
+import * as authActions from '../store/actions/auth';
+
+const StartupScreen = (props) => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const tryLogin = async () => {
+      const userData = await AsyncStorage.getItem('userData');
+      if (!userData) {
+        props.navigation.navigate('Auth');
+        return;
+      }
+      const parsedData = JSON.parse(userData);
+      const { token, userId, expirationDate } = parsedData;
+      const formattedExpiration = new Date(expirationDate);
+
+      if (formattedExpiration <= new Date() || !token || !userId) {
+        props.navigation.navigate('Auth');
+        return;
+      }
+      props.navigation.navigate('Shop');
+      dispatch(authActions.authenticate(userId, token));
+    };
+    tryLogin();
+  }, [dispatch]);
+  return (
+    <View>
+      <ActivityIndicator size="large" color={Colors.primary} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default StartupScreen;
